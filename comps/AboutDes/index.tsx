1d29e36216a510ed546aa195887503aad6e97dd8
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2023-02-02 10:52:19 -0800

    update description

--- comps/AboutDes/index.tsx
+++ comps/AboutDes/index.tsx
@@ -5,9 +5,9 @@ const AboutDes = () => {
           <div className="descrip-container">
               <div className='nb-text-tag'>{'<p>'}</div>
               <div className="descrip-text">
-                  <div className="descrip-text-purple">Enthusiastic in solving problems and learning new technology.</div>
-                  <div className="descrip-text-orange">Full-stack developer with two years of experience.</div>
-                  <div className="descrip-text-blue">Fan of vintage film camera.</div>
+                  <div className="descrip-text-purple">A driven problem-solver with a passion for continuous learning and improvement.</div>
+                  <div className="descrip-text-orange">Possesses a strong foundation in software engineering principles and stay current with industry trends.</div>
+                  <div className="descrip-text-blue">Committed to utilizing skills and knowledge to tackle challenges and drive business success.</div>
               </div>
               <div className='nb-text-tag'>{'</p>'}</div>
           </div>
